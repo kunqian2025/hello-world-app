c2e2a39aac3fc675edc573ee94fc3e85b10690b2
kunqian <35163742+kunqian2025@users.noreply.github.com>
2025-03-09 12:50:01 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+// sw.js
+const CACHE_NAME = 'hello-world-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
